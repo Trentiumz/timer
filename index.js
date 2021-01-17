255d896d5d51ad21c2a8e5bd378ecb4e0ba41aee
Daniel Ye <danielyeqt@gmail.com>
2021-01-17 15:19:11 -0500

    Create Basic Timer

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getTime(){
+  return new Date().getTime();
+}
+
+
 console.log("testtt");
+
+var currentTime;
+var alarmTime;
+var timeText;
+window.onload = function(){
+  currentTime = getTime();
+  alarmTime = currentTime + 30 * 60 * 1000;
+  console.log(currentTime);
+  timeText = document.getElementById("timer");
+  setInterval(changeTime, 80);
+}
+
+function changeTime(){
+  difference = Math.floor((alarmTime - getTime()) / 1000);
+  minutes = "" + Math.floor(difference / 60);
+  seconds = "" + (difference - minutes * 60);
+  timeText.innerHTML = minutes + ":" + seconds;
+}
