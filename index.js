413aacb65fb760adf9667544fd0c559145034efa
Daniel Ye <danielyeqt@gmail.com>
2021-01-17 15:29:20 -0500

    Add Sound, Remove Debug

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
+// Play sound function
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
+
+// get the time
 function getTime(){
   return new Date().getTime();
 }
 
-
-console.log("testtt");
-
-var currentTime;
 var alarmTime;
 var timeText;
+var timerIntervalInMS = 30 * 1000;
+var alarmSound;
+
 window.onload = function(){
-  currentTime = getTime();
-  alarmTime = currentTime + 30 * 60 * 1000;
-  console.log(currentTime);
+  alarmTime = getTime() + timerIntervalInMS;
   timeText = document.getElementById("timer");
+  alarmSound = new sound("alarm.mp3");
   setInterval(changeTime, 80);
 }
 
 function changeTime(){
   difference = Math.floor((alarmTime - getTime()) / 1000);
+  if(difference <= 0){
+    timerDone();
+  }
   minutes = "" + Math.floor(difference / 60);
-  seconds = "" + (difference - minutes * 60);
+  seconds = (difference - minutes * 60);
+  if(seconds < 10){
+    seconds = "0" + seconds;
+  }
   timeText.innerHTML = minutes + ":" + seconds;
+}
+
+function timerDone(){
+  alarmSound.play();
+  alarmTime = getTime() + timerIntervalInMS;
 }
